016433364e1c8554de26765ac6c620d2c231eaad
Cvetan Rusenov <crusenov@gmail.com>
2014-03-17 11:37:32 -0700

    Initial commit of function unique_words_count

--- week0/sp_2/b2_magic_string/magic_string.py
+++
@@ -1,5 +0,0 @@
-def magic_string(s):
-  """Vryshtame dyljinata na spisyka, v koito dobavqme element, samo ako
-  vyv vhodniq string s ima character, koito ne si e na mqstoto"""
-  return len([i for i, c in enumerate(s) if i < len(s) // 2 and c == '<'
-              or i >= len(s) // 2 and c == '>'])
