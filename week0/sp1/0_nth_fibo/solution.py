bd9053fe2372450b0e20447ed5d5b246759e023e
Cvetan Rusenov <crusenov@gmail.com>
2014-03-23 10:14:26 -0700

    Initial commit of function simplify_fraction

---
+++ week0/sp1/0_nth_fibo/solution.py
@@ -0,0 +1,44 @@
+def nth_fibonacci( n ):
+	a = 1
+	b = 1
+	ind = 2
+	while ind < n:
+		next = a + b
+		a = b
+		b = next
+		ind +=1 
+	return b
+
+
+
+
+
+
+	
+
+
+ 
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
--- week0/sp1/0_nth_fibo/tests.py
+++ week0/sp1/0_nth_fibo/tests.py
@@ -1,19 +1,19 @@
-import nth_fibonacci
+import solution
 import unittest
 
 class Nth_fibonacci(unittest.TestCase):
 
 	def test_nth_fibonaci_1(self):
-		self.assertEqual(1, nth_fibonacci.nth_fibonacci(1))
+		self.assertEqual(1, solution.nth_fibonacci(1))
 
 	def test_nth_fibonacci_2(self):
-		self.assertEqual(1, nth_fibonacci.nth_fibonacci(2))
+		self.assertEqual(1, solution.nth_fibonacci(2))
 
 	def test_nth_fibonacci_3(self):
-		self.assertEqual(2, nth_fibonacci.nth_fibonacci(3))
+		self.assertEqual(2, solution.nth_fibonacci(3))
 
 	def test_nth_fibonacci_10(self):
-		self.assertEqual(55, nth_fibonacci.nth_fibonacci(10))
+		self.assertEqual(55, solution.nth_fibonacci(10))
 
 if __name__ == '__main__':
     unittest.main()
