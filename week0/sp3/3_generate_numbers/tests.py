fc5a3d76e8666fc4df24a8446978f07164a91d7a
Cvetan Rusenov <crusenov@gmail.com>
2014-03-23 10:19:17 -0700

    Initial commit of function generate_numbers

---
+++ week0/sp3/3_generate_numbers/tests.py
@@ -0,0 +1,12 @@
+import solution
+import unittest
+
+
+class GenerateNumbersTest(unittest.TestCase):
+
+    def test_generate_numbers_1(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
